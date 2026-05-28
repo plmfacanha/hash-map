cd8bc2c6d4fee2b726f116b7f75e126a202791da
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-05-28 15:40:33 -0700

    First commit

---
+++ HashMap.js
@@ -0,0 +1,20 @@
+class HashMap {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+    this.bucket = new Array(this.capacity);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+}
+
+const hashMap = new HashMap();
